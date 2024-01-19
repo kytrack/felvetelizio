6b364beab25710784e4842741dee356fa1080cd3
vedran.krisztian <vedran.krisztian@CAD2-14>
2024-01-19 11:04:28 +0100

    Add project files.

---
+++ WpfApp1/Felvetelizo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Felvetelizo : IFelvetelizo
+    {
+        String OM_Azonosito;
+        String neve;
+        String ertesitesiCime;
+        String email;
+        DateTime szuletesiDatum;
+        int matematika;
+        int magyar;
+
+
+    }
+}
